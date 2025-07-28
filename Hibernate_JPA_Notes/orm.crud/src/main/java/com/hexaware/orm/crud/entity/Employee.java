606600630d59e11e86ad763174a021f6ece71acc
javeed512 <javeedmohammedjaveed@gmail.com>
2025-07-28 17:16:58 +0530

    Hibernate , JPA  Setup , CRUD Operations , pdfs , demos

---
+++ Hibernate_JPA_Notes/orm.crud/src/main/java/com/hexaware/orm/crud/entity/Employee.java
@@ -0,0 +1,55 @@
+package com.hexaware.orm.crud.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Emp_Table")
+public class Employee {
+	
+	@Id
+	private int eid;
+	@Column(name="emp_name")
+	private String ename;
+	private  double salary;
+	
+	public Employee() {
+		
+		
+	}
+	
+	
+	public Employee(int eid, String ename, double salary) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.salary = salary;
+	}
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", salary=" + salary + "]";
+	}
+	
+	
+
+}
