00b2e2303c9fb0946d405293a2d8b7eed27c037a
javeed512 <javeedmohammedjaveed@gmail.com>
2025-08-18 13:31:09 +0530

    ES6 PDF , ReactJS 1st Project , Notes

---
+++ React_Notes_Demos/Day1/src_component_intro/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+import MyButton  from './MyButton';
+
+function App(){  // App is a function component
+
+
+  return  <div>
+          <MyButton/> 
+          <MyButton/> 
+          <MyButton/> 
+
+
+          </div>
+  
+         
+
+
+
+}
+
+export  default App;
---
+++ React_Notes_Demos/Day1/src_component_intro/MyButton.jsx
@@ -0,0 +1,11 @@
+import {  StrictMode } from "react"
+
+
+
+ const MyButton =  ()=>{  //  arrow function component
+
+
+            return  <button >MyButton</button>;
+   }
+
+   export default MyButton;
